85def750d438f9aeca8f76b36ed2162352125acc
DIYANAPV <73051542+DIYANAPV@users.noreply.github.com>
2024-12-06 18:40:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,33 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="SemanticNgram",
-    version="0.1.0",
-    author="Diyana Muhammed",
-    author_email="diyanapv@gmail.com",
-    description="A semantic language model package with n-gram support and Word2Vec integration",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/DIYANAPV/Semantic_N-gram",
-    packages=find_packages(include=["SemanticNgram", "SemanticNgram.*"]),  # Includes subpackages like `download_word2vec`
-    include_package_data=True,  # Ensures non-Python files are included
-    package_data={
-        "SemanticNgram": ["model/*"],  # Includes files in the `model` folder
-    },
+    name="Semantic_Ngram",  # Name of your package
+    version="0.1",  # Initial version of your package
+    author="diyana",  # Author of the package
+    author_email="diyanapv@gmail.com",  # Author's email
+    description="A package for semantic n-gram models with Word2Vec",  # Short description of the package
+    long_description=open('README.md').read(),  # Detailed description (from README.md)
+    long_description_content_type='text/markdown',  # Markdown format for README
+    url="https://github.com/yourusername/Semantic_Ngram",  # URL to your GitHub repository
+    packages=find_packages(),  # This will automatically find all sub-packages
+    install_requires=[  # List of dependencies your package needs
+        'spacy',
+        'gensim',
+        'nltk',
+        'scipy',
+        'tqdm',
+        'requests'
+    ],
+    python_requires='>=3.6',  # Minimum Python version required
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: MIT License",  # You can change this to your license
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        "spacy",
-        "gensim",
-        "nltk",
-        "scipy",
-        "numpy",
-        "tqdm",  # Add tqdm for progress bar support
-        "requests",  # Add requests for downloading files
-    ],
-    python_requires=">=3.7",
+    include_package_data=True,  # Includes non-Python files specified in MANIFEST.in (if any)
+    entry_points={  # If you have CLI commands, define them here
+        'console_scripts': [
+            'semantic-ngram=SemanticNgram.main:main',  # Example command (adjust based on your entry point)
+        ],
+    },
 )
