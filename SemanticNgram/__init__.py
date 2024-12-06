23c7b9649d2c8fb6990c2d8a1fac9792a39f7bc6
DIYANAPV <73051542+DIYANAPV@users.noreply.github.com>
2024-12-06 19:09:44 +0100

    Update __init__.py

--- SemanticNgram/__init__.py
+++ SemanticNgram/__init__.py
@@ -3,4 +3,8 @@
 # Importing the main classes from Semantic_model.py to make them available at the package level.
 from .Semantic_model import SemanticLanguageModel, SemanticUnigramModel, SemanticNgramModel
 
+# SemanticNgram/__init__.py
+from .download_word2vec.download_word2vec import download_word2vec
+
+
 # You can add more imports here if you have other modules or components in this package
