8a7a05ea04c44669fa6b26f3b1a209758096ed04
DIYANAPV <73051542+DIYANAPV@users.noreply.github.com>
2024-12-06 18:42:51 +0100

    Update __init__.py

--- SemanticNgram/download_word2vec/__init__.py
+++ SemanticNgram/download_word2vec/__init__.py
@@ -1,1 +1,4 @@
-from .your_script import download_word2vec
+from SemanticNgram.download_word2vec import download_word2vec
+
+# Now you can use the function
+download_word2vec(destination_folder="your_model_folder")
