ca391c0bc0cfe528410de6e5c3307cdafb08bdd4
DIYANAPV <73051542+DIYANAPV@users.noreply.github.com>
2024-12-08 23:39:15 +0530

    Update __init__.py

--- SemanticNgram/__init__.py
+++ SemanticNgram/__init__.py
@@ -1,6 +1,6 @@
 # Importing the main classes from Semantic_model.py to make them available at the package level
 from .Semantic_model import SemanticLanguageModel, SemanticUnigramModel, SemanticNgramModel
-from .semantic_model import semantic_model_predict
+from .Semantic_model import semantic_model_predict
 
 
 
