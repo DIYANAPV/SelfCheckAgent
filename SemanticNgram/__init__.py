f9b6fb7970eb64b8ddaab4c5c006c5af9319040f
DIYANAPV <73051542+DIYANAPV@users.noreply.github.com>
2024-12-08 22:45:52 +0530

    Update __init__.py

--- SemanticNgram/__init__.py
+++ SemanticNgram/__init__.py
@@ -1,20 +1,4 @@
-# Import necessary modules
-import os
-
-# Importing the main classes from Semantic_model.py to make them available at the package level.
+# Importing the main classes from Semantic_model.py to make them available at the package level
 from .Semantic_model import SemanticLanguageModel, SemanticUnigramModel, SemanticNgramModel
-
-# Import necessary functions from download_word2vec
-from .download_word2vec.download_word2vec import download_word2vec
-
-# Define the path for the downloaded model
-model_path = "model/GoogleNews-vectors-negative300.bin"
-
-# Only download if the model doesn't already exist
-if not os.path.exists(model_path):
-    print("Word2Vec model not found. Downloading...")
-    download_word2vec(destination_folder="model")
-else:
-    print("Word2Vec model already exists.")
 
 
