805d298689e09d7421d64e14191c82e89d0af1b2
DIYANAPV <73051542+DIYANAPV@users.noreply.github.com>
2024-12-09 10:15:18 +0100

    Delete SemanticNgram/download_resources.py

--- SemanticNgram/download_resources.py
+++
@@ -1,9 +0,0 @@
-import gensim.downloader as api
-
-def download_word2vec():
-    print("Downloading Word2Vec model...")
-    api.load("word2vec-google-news-300")  # This automatically caches the model in ~/.gensim-data/
-    print("Word2Vec model downloaded and cached.")
-
-if __name__ == "__main__":
-    download_word2vec()
