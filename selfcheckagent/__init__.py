071820dbb1c715d5e181bc591905f22bd362354e
DIYANAPV <73051542+DIYANAPV@users.noreply.github.com>
2024-12-17 17:51:45 +0100

    Update __init__.py

--- selfcheckagent/__init__.py
+++ selfcheckagent/__init__.py
@@ -1,15 +1,19 @@
-from .contextual_agent import SelfCheckNLI
+# selfcheckagent/__init__.py
+
+from .contextual_agent import ContextualAgent
+from .specialized_agent import SelfCheckNLI
 from .symbolic_agent import (
-    SemanticLanguageModel, 
-    SemanticUnigramModel, 
+    SemanticLanguageModel,
+    SemanticUnigramModel,
     SemanticNgramModel,
     semantic_model_predict
 )
 
 __all__ = [
-    "SelfCheckNLI", 
-    "SemanticLanguageModel", 
-    "SemanticUnigramModel", 
-    "SemanticNgramModel", 
+    "ContextualAgent",
+    "SelfCheckNLI",
+    "SemanticLanguageModel",
+    "SemanticUnigramModel",
+    "SemanticNgramModel",
     "semantic_model_predict"
 ]
