872dd9106b2da6b5274456745eade8ef6d6948b4
DIYANAPV <73051542+DIYANAPV@users.noreply.github.com>
2024-12-17 10:37:12 +0100

    Delete SemanticNgram directory

--- SemanticNgram/__init__.py
+++
@@ -1,6 +0,0 @@
-# Importing the main classes from Semantic_model.py to make them available at the package level
-from .Semantic_model import SemanticLanguageModel, SemanticUnigramModel, SemanticNgramModel
-from .Semantic_model import semantic_model_predict
-
-
-
