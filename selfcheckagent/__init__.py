8615f07de1e28faffa5f7e31e1429232823f6b3a
DIYANAPV <73051542+DIYANAPV@users.noreply.github.com>
2024-12-17 10:37:32 +0100

    Create __init__.py

---
+++ selfcheckagent/__init__.py
@@ -0,0 +1,3 @@
+# Importing the main classes from Semantic_model.py to make them available at the package level
+from .Semantic_model import SemanticLanguageModel, SemanticUnigramModel, SemanticNgramModel
+from .Semantic_model import semantic_model_predict
